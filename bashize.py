3adb8511e2762031ca442b072fe90a0d286cc764
Gian-Carlo DeFazio <defazio1@llnl.gov>
2020-08-05 18:03:50 -0700

    added parser for lustre_paths.py that is called by lu.py

--- bashize.py
+++ bashize.py
@@ -12,24 +12,24 @@ import subprocess
 import sys
 import uuid
 
-# could deal with race conditions if you 
+# could deal with race conditions if you
 # really like pain, make an atomic file modification
 # function
 def bashize(path):
     '''Create a bash script at the given path.
     If no file exists, create one.
-    If a file does exist but does not start with 
+    If a file does exist but does not start with
     #!, add #!/bin/bash and make executable,
     if #! already exists, do nothing.
     '''
     path = pathlib.Path(path)
 
     # get bash's path
-    which_bash_path = subprocess.run(['which', 'bash'], 
+    which_bash_path = subprocess.run(['which', 'bash'],
                                      capture_output=True,
                                      check=True)
     shebangbash = '#!' + which_bash_path.stdout.decode() + '\n'
-    
+
     # ensure something exists at path
     path.touch()
 
@@ -37,13 +37,13 @@ def bashize(path):
     if not path.is_file():
         raise FileNotFoundError(str(path) + ' is not normal a'
                                     ' normal file, cannot bashize')
-        
+
     # check if #! line already exists
     # in which case there's nothing to do
     with open(path, 'rb') as f:
         first_2 = f.read(2)
         if first_2 == b'#!':
-            print(str(path) + ' already starts with \'!#', 
+            print(str(path) + ' already starts with \'!#',
                   file=sys.stderr)
             return
 
@@ -57,5 +57,3 @@ def bashize(path):
     # overwrite and set permisssions
     shutil.move(f_name, path)
     path.chmod(0o744)
-        
-
--- lustre_paths.py
+++ lustre_paths.py
@@ -2,28 +2,91 @@ __doc__ = '''
 Find lustre on the system.
 '''
 
+from collections import OrderedDict
 from pathlib import Path
+import set_colors
+import argparse
 
-# places luste is likely to be
-default_lustre_paths = [
+
+parser_help =('get a path within the lustre source code. '
+            'This command will attempt to find the root of the lustre source code '
+            'using user given base names or default base name. '
+            'The check to make sure that the given base name is actually lustre is very weak '
+            'it just checks if the path is a non-empty directory. '
+            'Then a relative path will be added to give the full name.')
+
+
+def set_up_parser(parser=None):
+    # parser for this script alone
+    if parser is None:
+        parser = argparser.ArgumentParser(help=parser_help)
+    parser.add_argument('-s', '--show', action='store_true',
+        help='show default base paths and relative paths.')
+    parser.add_argument('-b', '--base_paths', nargs='*',
+        help='set base paths, these paths will be used as the root of the lustre source code '
+        'if they are non-empty directories. If none of the given base paths '
+        'is plausibly the root of the lustre source code, the default base '
+        'paths will be checked next, unless --default is set to no.')
+    parser.add_argument('-d', '--default', choices=['yes', 'no', 'first'],
+        help='control the order in which base paths are checked. '
+        'if \'yes\' the default base paths will be checked after any given base paths. '
+        'if \'no\' the default base paths will not be checked. '
+        'if \'first\', the default base paths will be checked before any given base paths '
+        'the default is \'yes\'')
+    parser_mutex = parser.add_mutually_exclusive_group()
+    parser_mutex.add_argument('-n', '--name',
+        help='the name of some path in the lustre source tree. '
+        'It will br translated into a path relative to the base path '
+        'if it is a known name. To see the known names use --show.')
+    parser_mutex.add_argument('-r', '--relative', nargs='?', const='',
+        help='the relative path name. This will be added onto '
+        'the lustre root path making <lustre source root>/<relative path>')
+        
+
+
+    
+
+
+# places lustre is likely to be
+default_lustre_paths = list(OrderedDict.fromkeys([
     Path.home() / 'lustre-release',
     Path.home() / 'lustre',
-]
+    Path('/g/g0/defazio1') / 'lustre-release',
+    Path('/g/g0/defazio1') / 'lustre',
+]))
 
 # important relative paths within lustre
 relative_paths = {
+    'lustre' : '',
     'lctl' : 'lustre/utils/lctl',
     'lfs' : 'lustre/utils/lfs',
     'llmount' : 'test/llmount.sh',
     'llmountcleanup' : 'test/llmountcleanup.sh',
 }
 
-def find_lustre(paths=None, check_defaults=True, check_defaults_first=False):
+
+def show_paths():
+    '''Show all default base paths,  as well as relative path names
+    and their corresponding paths.'''
+    set_colors.print_rgb((125,56,179), 'default base paths:')
+    for p in default_lustre_paths:
+        print(str(p))
+    set_colors.print_rgb((125,56,179), 'relative paths:')
+    for n,p in relative_paths.items():
+        print(n + ' -> \'' + p + '\'')
+
+def find_lustre(paths=None, check_defaults=None, check_defaults_first=None):
     '''Check file paths for likely root of lustre source
     the first that exists is considered to be lustre.
     Looks in possible_lustre_paths by default. Will also look in paths
     if given.
     '''
+    # set optional values
+    if check_defaults is None:
+        check_defaults = True
+    if check_defaults_first is None:
+        check_defaults_first = False
+
     # select which paths to check and in which order
     all_paths = []
     if paths:
@@ -46,6 +109,61 @@ def lustre_paths(lustre_path):
                     for name, relative_path
                     in relative_paths.items()}
 
+def find_path(lustre_path, rel_path_name):
+    '''Try to find a single lustre path'''
+    rel_path = relative_paths.get(rel_path_name)
+    if rel_path is None:
+        return None
+    return lustre_path / rel_path
+
+def main(args):
+    if args['show']:
+        show_paths()
+
+    base_paths = args.get('base_paths')
+    if base_paths == []:
+        base_paths = None
+
+    default = args.get('default')
+    relative = args.get('relative')
+    check_defaults = None
+    check_defaults_first = None
+    if default == 'yes':
+        check_defaults = True
+        check_defaults_first = False
+    if default == 'first':
+        check_defaults = True
+        check_defaults_first = True
+    if default == 'no':
+        check_defaults = False
+        check_defaults_first = False
+    lustre_root = find_lustre(base_paths, 
+                              check_defaults,
+                              check_defaults_first)
+
+    # mutally exclusive based on parser
+    if args['name'] is not None:
+        full_path = find_path(lustre_root, args['name'])
+        if full_path is not None:
+            print(full_path)
+    if args['relative']:
+        print(lustre_root / relative)
+    
+        
+
+    # paths = args.get('paths')
+    # check_defaults = args.get('check_defaults')
+    # check_defaults_first = args.get('check_defaults_first')
+    # lustre_path = find_lustre(paths, check_defaults, check_defaults_first)
+    # print(lustre_path)
+    
+
+
+
+
+
+if __name__ == '__main__':
+    pass
 
 
 
