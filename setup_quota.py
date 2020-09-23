188c68c7969bf4b8412216d91bbf554d6ebaf677
Gian-Carlo DeFazio <defazio1@llnl.gov>
2020-09-23 13:35:56 -0700

    executable version of lu, setup_quota.py extended
    setup_quota_small.py will eventually be absorbed
    into setup_quota.py

--- setup_quota.py
+++ setup_quota.py
@@ -1,51 +1,191 @@
+import argparse
+import pathlib
+import re
 import subprocess
 
-import set_colors
-import lustre_paths
-import lustre_utils
+# TODO, this is limited to 192.168 ... addresses, which may not work for different setups
+# should grab the correct ip regardless
+# TODO hostname can be gotten from the environment
+def set_ip(hostname='vmlustre'):
+    '''Set the ip address in /etc/hosts to 
+    match that of ifconfig. This is for lustre/tests/llmount.sh'''
 
-# prerequisites:
-# expects lustre installed using llmount.sh at /mnt/lustre
+    # need python 3.7 for capture_output
+    # ip_data = subprocess.run(['ifconfig'], 
+    #                          capture_output=True).decode()
 
-# get paths
-lustre = str(lustre_paths.find_lustre())
-lfs = str(lustre_paths.find_lustre(lr, 'lfs'))
-lctl = str(lustre_paths.find_lustre(lr, 'lctl'))
+    # get ip address of this machine from ifconfig    
+    ip_data = subprocess.run(['ifconfig'], 
+                             stdout=subprocess.PIPE).stdout.decode()
+    ifconfig_ip = ip4_pattern.search(ip_data).group('ip4')
+    if ifconfig_ip is None:
+        raise Exception('no usable ip in ifconfig')
 
-# verify this is the right machine, slag3 by default
-check_machine('slag3')
+    # now get the ip address in /etc/hosts for hostname
+    # also save the contents of /etc/hosts because it might
+    # be needed later
+    etc_ip = None
+    hosts = None
+    with open('/etc/hosts', 'r') as f:
+        hosts = f.read()
 
-# make /mnt/lustre writeable
-os.chmod('/mnt/lustre', 0777)
+    for line in hosts.split('\n'):
+        if hostname in line:
+            etc_ip = ip4_pattern.match(line).group('ip4')
 
-# set lustre debug logs to quotas
-subprocess.run([lctl, 'set_param', 'debug=-1'],
-               capture_output=True,
-               check=True)
-subprocess.run([lctl, 'set_param', 'subsytem_debug=-lquota'],
-               capture_output=True,
-               check=True)
+    if etc_ip is None:
+        raise Exception('no usable ip in /etc/hosts')
 
-# turn on quotas for osts and mdts
-subprocess.run([lctl, 'conf_param', 'lustre.quota.ost=u'],
-               capture_output=True,
-               check=True)
-subprocess.run([lctl, 'conf_param', 'lustre.quota.mdt=u'],
-               capture_output=True,
-               check=True)
+    # if the are equal, nothing to do
+    if ifconfig_ip == etc_ip:
+        return
+
+    # replace etc_ip in /etc/hosts with ifconfig_ip
+    # NOTE this assumes that this ip appears only once
+    # or this might break things
+    hosts = hosts.replace(etc_ip, ifconfig_ip)
+
+    # overwrite /etc/hosts
+    with open('/etc/hosts', 'w') as f:
+        f.write(hosts)
+        
+
+# TODO maybe get user_name from environment
+def setup_quota(fs_path='/mnt/lustre', user_name='defazio1'):
+    '''set up lustre for quotas.
+    '''
+    # make the lustre file system writable
+    mnt = pathlib.Path(fs_path)
+    mnt.chmod(0o777)
+
+    # set up logging for quotas
+    subprocess.run([lctl_path, 'set_param', 'debug=-1'])
+    subprocess.run([lctl_path, 'set_param', 'subsystem_debug=lquota'])
+
+    # turn on quotas for osts and mdts for users
+    subprocess.run([lctl_path, 'conf_param', 'lustre.quota.ost=u'])
+    subprocess.run([lctl_path, 'conf_param', 'lustre.quota.mdt=u'])
+
+    # set grace periods for block and inode for users
+    subprocess.run([lfs_path, 'setquota', '-t', '-u', '--block-grace', 
+                    '20w4d12h3m13s', '--inode-grace', '7200', '/mnt/lustre'])    
+
+    # set block limits for the user
+    subprocess.run([lfs_path, 'setquota', '-u', user_name, 
+                    '--block-softlimit', '20M', 
+                    '--block-hardlimit', '30M', 
+                    '/mnt/lustre'])
+    subprocess.run([lfs_path, 'setquota', '-u', user_name, 
+                    '--inode-softlimit', '2048', 
+                    '--inode-hardlimit', '3072', 
+                    '/mnt/lustre'])
+    
+def mount_lustre():
+    '''check if lustre is mounted, if not,
+    mount it.'''
+    lsmod_output = subprocess.run(['lsmod'], 
+                                stdout=subprocess.PIPE).stdout.decode()
+    if 'lustre' not in lsmod_output:
+        # need to run llmount
+        subprocess.run([llmount_path])
+
+def unmount_lustre():
+    '''check if lustre is mounted, if so, unmount it'''
+    lsmod_output = subprocess.run(['lsmod'], 
+                                stdout=subprocess.PIPE).stdout.decode()
+    if 'lustre' in lsmod_output:
+        # need to run llmount
+        subprocess.run([llmountcleanup_path] )
+        
+    
+def build_lustre(user_name='defazio1'):
+    # build as defazio1
+    subprocess.run(['sudo', '-u', user_name, 'make', 'clean'], cwd=lustre_root)
+    subprocess.run(['sudo', '-u', user_name, 'sh', 'autogen.sh'], cwd=lustre_root)
+    subprocess.run(['sudo', '-u', user_name, './configure'], cwd=lustre_root)
+    subprocess.run(['sudo', '-u', user_name, 'make', '-j12'], cwd=lustre_root)
 
 
+def setup_quota_test():
+    '''assumes no lustre build or installed'''
+    #build_lustre()
+    #set_ip()
+    #mount_lustre()
+    setup_quota()
 
-# set 1 min grace period for block and inode for users
-subprocess.run([lctl, 'conf_param', 'lustre.quota.mdt=u'],
-               capture_output=True,
-               check=True)
+def resetup_quota_test(full=False, user_name='defazio1'):
+    '''assumes lustre needs to be remade and reinstalled'''
+    unmount_lustre()
+    if full:
+        subprocess.run(['sudo', '-u', user_name, 'make', 'clean'], cwd=lustre_root)
+        subprocess.run(['sudo', '-u', user_name,'sh', 'autogen.sh'], cwd=lustre_root)
+        subprocess.run(['sudo', '-u', user_name,'./configure'], cwd=lustre_root)
+    subprocess.run(['sudo', '-u', user_name,'make', '-j12'], cwd=lustre_root)
+    mount_lustre()
+    setup_quota()
 
 
-$LFS setquota -t -u --block-grace 20w4d12h3m13s --inode-grace 7200 /mnt/lustre
+# the usage string for this command
+# it will be used as the main usage string
+# if this command is called directly,
+# or as the subcommand help string if called indirecly by lu
+# required for use by lu
+parser_help = '''Some arguments need sudo, this is specified
+per argument.
+'''
 
-# set block limits for me
-$LFS setquota -u defazio1 --block-softlimit 20M --block-hardlimit 30M /mnt/lustre
+# a wrapper for set_up_parser_local
+# allows for the case that an existing parser is
+# being modified (which is the case when lu is using this
+# module for a subcommand) and the case where this module
+# is run as main, and no parser yet exists
+def set_up_parser(parser=None):
+    if parser is None:
+        parser = argparse.ArgumentParser(usage=parser_help)
 
-# set inode limits for me
-$LFS setquota -u defazio1 --inode-softlimit 2048 --inode-hardlimit 3072 /mnt/lustre  
+    # lu expects the parser to a field called 'func'
+    # which is set to 'main' by default
+    parser.set_defaults(func=main)
+    set_up_parser_local(parser)
+    return parser
+
+# set up the parser for this command
+# the parser passed in is either one passed from
+# lu, or one just created for this module
+# part of all this infrastructure is to hopefully make
+# it not matter what the context is, so this function
+# can be written as if it's just for this module being
+# called directly but it will work for lu as well
+def set_up_parser_local(parser):
+    parser.add_argument('-s', '--set-ip', action='store_true',
+                        help=('Requires sudo.\n'
+                              'put an entry in /etc/hosts with '
+                              'the hostname and the host ip from ifconfig. '
+                              'this is needed for lustre test setup llmount.sh'))
+    parser.add_argument('-n', '--name',
+                        help=('the hostname, this will be '
+                              'the environment variable $HOSTNAME '
+                              'by default.'))
+    parser.add_argument('-m', '--mount',
+                        help=('Requires sudo.\n'
+                        'mount lustre of not already mounted '
+                        'using lustre test setup llmount.sh'))
+    parser.add_argument('-u', '--unmount',
+                        help=('Requires sudo.\n'
+                              'unmount lustre if mounted using '
+                              'lustre test setup llmountcleanup.sh'))
+)
+    parser.add_argument('-r', '--remount')    
+    
+    
+
+# the function that actually executes the command
+# this function deals with the arguments after they are
+# parsed and are made into a dictionary
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    parser = set_up_parser()
+    args = vars(parser.parse_args())
+    main(args)
