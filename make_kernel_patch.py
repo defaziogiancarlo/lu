7772383f06742edff643b1fa5c6ea805e4624b32
Gian-Carlo DeFazio <defazio1@llnl.gov>
2020-11-07 02:06:26 +0000

    actually made patch file

--- make_kernel_patch.py
+++ make_kernel_patch.py
@@ -1,6 +1,8 @@
 '''not a subcommand, not meant to be '''
 
+import os
 import re
+import pathlib
 import subprocess
 
 import cfg
@@ -17,26 +19,29 @@ def make_patch():
                          check=True).stdout.decode().strip()    
     kv = re.match(version_pattern, version).group('kernel')
 
-    print(kv)
-
     # find the series file
     series_file_name = None
     with open(lustre_path / 'lustre/kernel_patches/which_patch', 'r') as f:
         for line in f.readlines():
             if kv in line:
                 series_file_name = line.split()[0]
-
-    print(series_file_name)
                 
     # read the series file, and get full paths to each patch file
-    series_path = lustre_path / 'lustre/kernel_patches' / series_file_name
+    series_path = lustre_path / 'lustre/kernel_patches/series' / series_file_name
     patch_files = None
-    with open(seried_path, 'r') as f:
-        patch_dir = lustre_path / 'kernel_patches/patches'
-        patch_files = [path_dir / line for line in f.readlines()]
+    with open(series_path, 'r') as f:
+        patch_dir = lustre_path / 'lustre/kernel_patches/patches'
+        patch_files = [patch_dir / line.strip() for line in f.readlines()]
 
 
-    print(patch_files)
+    home_dir = os.environ['HOME']
+    rpm_src_patch = pathlib.Path(home_dir) / 'kernel/rpmbuild/SOURCES/patch-lustre.patch'
+    with open(rpm_src_patch, 'w') as out_file:
+        for patch_file in patch_files:
+            text = None
+            with open(patch_file, 'r') as in_file:
+                text = in_file.read()
+            out_file.write(text)
     
 
 if __name__ == '__main__':
